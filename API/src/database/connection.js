cb24dc65d0b81bf30ef9512fc7c609c9ade9c059
Lowell Pence <lxp3901@rit.edu>
2021-10-19 17:39:50 -0400

    - altered connection string

--- API/src/database/connection.js
+++ API/src/database/connection.js
@@ -5,7 +5,7 @@ export function getConnection() {
         host: "localhost",
         port: 3306,
         user: "root",
-        password: "neonflamingo",
+        password: "",
         database: "iste500"
     });
 }
