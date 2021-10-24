57d9f718740c629dcfe60792db4385d82c0c8341
Lowell Pence <lxp3901@rit.edu>
2021-10-24 16:11:27 -0400

    enable jwt auth

--- API/app.js
+++ API/app.js
@@ -1,5 +1,5 @@
 import express from 'express';
-//import jwt from 'express-jwt';
+import jwt from 'express-jwt';
 import reasonRouter from './src/route/reason.js';
 import meetingHostRouter from './src/route/advisor.js';
 import studentRouter from './src/route/student.js';
@@ -11,14 +11,14 @@ const port = 8080;
 const app  = express();
 
 // Add JWT Authorization middleware first
-// app.use(jwt({ secret: 'ischool' }));
-// app.use((err, req, res, next) => {
-//     if (err.name === 'UnauthorizedError') {
-//       res.status(err.status).send({ message: err.message });
-//       return;
-//     }
-//     next();
-// });
+app.use(jwt({ secret: 'ischool', algorithms: ['HS256'] }));
+app.use((err, req, res, next) => {
+    if (err.name === 'UnauthorizedError') {
+      res.sendStatus(err.status).send({ message: err.message });
+      return;
+    }
+    next();
+});
 
 // forward request to the PHP script which will redirect to Shib auth
 // then it will return a JWT
